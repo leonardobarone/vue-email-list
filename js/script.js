d85b1660ff5f5082e410026e46392fa2a1ccbc88
leonardobarone <leonardobarone26@gmail.com>
2021-09-27 16:06:30 +0200

    email genearati

--- js/script.js
+++ js/script.js
@@ -3,14 +3,34 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const app = new Vue({
 
     el: "#root",
+    
     data: {
-
+        array: []
     },
+    
     methods: {
 
     },
 
+    mounted () {
+        
+        for (let i = 1; i <= 10; i++) {
+
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            
+                this.array.push(response.data.response)
+        
+        })
+
+        }
+
+    }
+
 });
+
