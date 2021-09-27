0b5526e2a9b1381b050aa84d767696c5540eee54
leonardobarone <leonardobarone26@gmail.com>
2021-09-27 17:48:45 +0200

    inseriti per prova button aggiungi e rimuovi

--- js/script.js
+++ js/script.js
@@ -9,25 +9,43 @@ const app = new Vue({
     el: "#root",
     
     data: {
-        array: []
+        array: [],
     },
     
     methods: {
+
+        aggiungi() {
+        
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                
+                    this.array.push(response.data)
+            
+            })
+
+        },
+
+        rimuovi() {
+            this.array.splice(this.array.length - 1, 1)
+        }
 
     },
 
     mounted () {
         
+        const self = this;
+
         for (let i = 1; i <= 10; i++) {
-
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            
-                this.array.push(response.data.response)
         
-        })
-
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( function(response) {
+                
+                    self.array.push(response.data)
+            
+            })
+        
         }
 
     }
